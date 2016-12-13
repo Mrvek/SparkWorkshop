725cc43df4bed017c8d0d7263536ff88f0e9bfad
Mitchell van Ek <mitchell.v.ek@xs4all.nl>
2016-12-13 16:00:13 +0100

    fixed websocket annotation

--- src/main/java/WebSockets/ChatSocketHandler.java
+++ src/main/java/WebSockets/ChatSocketHandler.java
@@ -1,10 +1,10 @@
 package WebSockets;
 
-import Domain.PLeerling.UserListHandler;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
@@ -14,6 +14,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 /**
  * Created by Mitchell on 13/12/2016.
  */
+
+@WebSocket
 public class ChatSocketHandler {
     private static final Queue<Session> sessions = new ConcurrentLinkedQueue<>();
 
