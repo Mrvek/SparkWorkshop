0fdd17f67a65459fa36c8349002dc5c8185a47e7
Victor <victor.duits@student.hu.nl>
2016-12-14 23:39:46 +0100

    Aanpassing opdracht 1 en 2

--- src/main/java/Main/AppBuilder.java
+++ src/main/java/Main/AppBuilder.java
@@ -18,8 +18,8 @@ public class AppBuilder {
     public static void setup() {
         try {
 //            TODO: Finish  assignmentbuilder, may be hardcoded
-            AssignmentHandler.createAssignment("assignment1", "maak een webserver die bij jouw poortnummer 'Hello World!' returned.");
-            AssignmentHandler.createAssignment("assignment2", "laat je webserver gebruik maken van index.html zodra je de website bezoekt");
+            AssignmentHandler.createAssignment("assignment1", "maak een webserver die bij jouw poortnummer 'Hello JouwVoorNaam!' returned.");
+            AssignmentHandler.createAssignment("assignment2", "laat je webserver jouw generated number terug returnen");
             AssignmentHandler.createAssignment("assignment3", "Maak een websocket op /ping en zorg ervoor dat hij elk bericht dat hij binnenkrijgt antwoord met 'pong'.");
             AssignmentHandler.createAssignment("assignment4", "Maak 2 websockets aan op /server en /front. Onze server stuurt berichten van iedereen door naar /server en deze moeten naar jouw front-end gaan met /front. Ook moet je zelf berichten kunnen sturen en naar /server kunnen sturen");
 
